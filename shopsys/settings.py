6a734af6062915509a1b417900dd5ae1c356c170
2279444100@qq.com <2279444100@qq.com>
2016-03-10 17:12:54 +0800

    更改数据取引擎

--- shopsys/settings.py
+++ shopsys/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'shopsys.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shopsys',
+        'USER':'shopsys',
+        'PASSWORD':'123456',
+        'HOST':'127.0.1',
+        'POST':'3306',
+        'TEXT'  :{ },
     }
 }
 
